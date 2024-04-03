b306a48e9a075b483a2472f84634ce0fb414f26b
JP Halog <jdh0102@auburn.edu>
2024-04-03 15:12:12 -0500

    Creating index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bookstore Database Interface</title>
+</head>
+<body>
+  <h1>Bookstore Database Interface</h1>
+  <form method="post" action="execute_query.php">
+    <label for="query">Enter SQL Query:</label><br>
+    <textarea id="query" name="query" rows="4" cols="50"></textarea><br>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
