095fcbd115daa4abd29ff56832cd66780b05bdee
JP-Halog <jdh0102@auburn.edu>
2024-04-23 16:42:07 -0500

    Finished updating tables

--- create_database.php
+++ create_database.php
@@ -5,7 +5,7 @@ $password = "Megabotx#1";
 $dbname = "jdh0102db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
@@ -13,19 +13,18 @@ if ($conn->connect_error) {
 }
 
 // Path to the .csv file
-$csvFile = 'db_book.csv';
+$csvFile = 'db_customer.csv';
 
 // Open the .csv file
 $fileHandle = fopen($csvFile, 'r');
 
-// Get the first line and determine the number of fields
-$firstLine = fgetcsv($fileHandle);
-$numFields = count($firstLine);
+// Get the first line and use it as the column names
+$columnNames = fgetcsv($fileHandle);
 
 // Create a table with the same number of fields
-$sql = "CREATE TABLE IF NOT EXISTS db_book (";
-for ($i = 0; $i < $numFields; $i++) {
-  $sql .= "field$i VARCHAR(255), ";
+$sql = "CREATE TABLE IF NOT EXISTS db_customer (";
+foreach ($columnNames as $columnName) {
+  $sql .= "$columnName VARCHAR(255), ";
 }
 $sql = rtrim($sql, ', ') . ")";
 if ($conn->query($sql) === TRUE) {
@@ -37,7 +36,7 @@ if ($conn->query($sql) === TRUE) {
 // Insert data from the .csv file into the table
 while (($line = fgetcsv($fileHandle)) !== FALSE) {
   $values = "'" . implode("', '", $line) . "'";
-  $sql = "INSERT INTO your_table_name VALUES ($values)";
+  $sql = "INSERT INTO db_customer VALUES ($values)";
   if ($conn->query($sql) === TRUE) {
     echo "Record inserted successfully\n";
   } else {
