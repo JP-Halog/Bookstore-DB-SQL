dd96a98d8320d206ad231c41c310bdfb309e45a1
JP Halog <jdh0102@auburn.edu>
2024-04-28 13:11:55 -0500

    Lots of changes

--- display_tables.php
+++ display_tables.php
@@ -14,20 +14,48 @@ if ($conn->connect_error) {
 
 // Get list of tables
 $sql = "SHOW TABLES";
-$result = $conn->query($sql);
+$tables_result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // Start a table
-    echo "<table border='1'>";
-    echo "<tr><th>Table Name</th></tr>";
+if ($tables_result->num_rows > 0) {
+    // Loop through each table
+    while($table_row = $tables_result->fetch_assoc()) {
+        $table_name = $table_row['Tables_in_' . $dbname];
+        echo "<h2>Table: $table_name</h2>";
+        
+        // Get table content
+        $content_sql = "SELECT * FROM $table_name";
+        $content_result = $conn->query($content_sql);
 
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row['Tables_in_' . $dbname] . "</td></tr>";
+        if ($content_result->num_rows > 0) {
+            // Start a table
+            echo "<table border='1'>";
+            
+            // Output headers
+            $fields = $content_result->fetch_fields();
+            echo "<tr>";
+            foreach ($fields as $field) {
+                echo "<th>" . $field->name . "</th>";
+            }
+            echo "</tr>";
+            
+            // Output data of each row
+            while($row = $content_result->fetch_assoc()) {
+                echo "<tr>";
+                foreach ($row as $cell) {
+                    echo "<td>" . $cell . "</td>";
+                }
+                echo "</tr>";
+            }
+            
+            // End the table
+            echo "</table>";
+        } else {
+            echo "Table is empty";
+        }
+        
+        // Add some space between tables
+        echo "<br>";
     }
-    
-    // End the table
-    echo "</table>";
 } else {
     echo "No tables found";
 }
