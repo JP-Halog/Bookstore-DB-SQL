636e396cbda6d58f1520f204b1ebdb21d7888386
JP Halog <jdh0102@auburn.edu>
2024-04-28 13:53:26 -0500

    Changed site name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <title>Bookstore Database Interface</title>
+  <title>JDH0102 JP Halog's Bookstore Database Interface</title>
   <style>
     body {
       background-color: #121212;
